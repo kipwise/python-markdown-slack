d44ecff573c1fe1b002a8f99b9a82ac8f71ecd29
Wing leung CHOI <wingleungchoi@gmail.com>
2018-07-06 00:08:41 +0800

    rename

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 setup(
     name='python-markdown-slack',
-    version='0.1',
-    py_modules=['python-markdown-slack'],
+    version='0.2',
+    py_modules=['python_markdown_slack'],
     install_requires = ['markdown>=2.5'],
     license='MIT'
 )
