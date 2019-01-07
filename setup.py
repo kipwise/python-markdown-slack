009d4d3ebad3cfb992e90b3b79be4ed83ee1dd85
Kwun-Lok Ng <k@samplify.io>
2019-01-07 17:16:41 +0200

    feat: update pkginfo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-markdown-slack',
-    version='0.7',
+    version='0.8',
     py_modules=['python_markdown_slack'],
     install_requires=['markdown>=2.5'],
     license='MIT'
