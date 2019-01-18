c89c4fd11948f03f0a61662c08b1f7df59da3544
Kwun-Lok Ng <k@samplify.io>
2019-01-18 16:24:21 +0200

    fix: fix a dependency issue

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='python-markdown-slack',
     version='1.0.1',
     py_modules=['python_markdown_slack', 'python_markdown_slack.list_handler'],
-    install_requires=['markdown>=2.5'],
+    install_requires=['markdown==2.6.6'],
     license='MIT'
 )
